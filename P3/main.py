9442f854246cb6548fd501f7dc2e35f2cb847626
Kawson <piegusek20@gmail.com>
2022-05-30 00:30:32 +0200

    Fix algorithm problem - A matrix.

--- P3/main.py
+++ P3/main.py
@@ -6,11 +6,10 @@ def Testy():
     # print(x2)
     # y = [26.0, 48.6, 12.6, 71.2, 74.8, 75.2,60.0]
     # x = [0,20,40,60,80,100,120]
-    x = [i for i in range(0, 9, 4)]
-    y = [6,-2, 4]
-    print(x)
+    x = [1, 3, 5]
+    y = [6, -2, 4]
     r = FunkcjaSklejania(x, y)
-    xx = [i for i in np.arange(0, len(r)*0.25, 0.25)]
+    xx = [i for i in np.arange(x[0], len(r)*0.25+x[0], 0.25)]
     # print(r)
     PlotData(x, y, "TEST", xx, r)
     N = 5
